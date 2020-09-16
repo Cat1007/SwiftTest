e970ed7e75e8dfa0da36c011be27789596191b26
Cat1007 <1409343997@qq.com>
2020-09-16 13:53:16 +0800

    Initial Commit

---
+++ SwiftTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftTest
+//
+//  Created by 叶锦浩 on 2020/9/16.
+//  Copyright © 2020 叶锦浩. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
